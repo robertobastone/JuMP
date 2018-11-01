bee1ecede9b02f8a0c2dd60db360749f67a56a32
Roberto Bastone <35118511+robertobastone@users.noreply.github.com>
2018-11-02 00:02:28 +0100

    Add files via upload

--- JuMP.jl
+++ JuMP.jl
@@ -3,9 +3,12 @@
 # author: Roberto Bastone
 # email: robertobastone93@gmail.com
 
-version = 1.0
-using AstroLib
+version = 1.1
 
+using AstroLib
+using Dates
+
+today = datetime2julian(Dates.today())
 # code
 
 println("Welcome to JUlia Moon Phase (JuMP) ", version)
@@ -23,28 +26,76 @@ jd = jdcnv(year, month, day)
 
 
 if 0 < AstroLib.mphase(jd)*100 < 1
-	println("On ",year,"/",month,"/",day," there was/is/will be a new moon.")
+	if today > jd
+		println("On ",year,"/",month,"/",day," there was a new moon.")
+	elseif today == jd
+		println("On ",year,"/",month,"/",day," there is be a new moon.")
+	elseif today < jd
+		println("On ",year,"/",month,"/",day," there will be a new moon.")
+	end
 elseif 1 <= AstroLib.mphase(jd)*100 < 49
 	if AstroLib.mphase(jd) > AstroLib.mphase(jd-1)
-		println("On ",year,"/",month,"/",day," there was/is/will be a waxing crescent.")
+		if today > jd 
+			println("On ",year,"/",month,"/",day," there was be a waxing crescent.")
+		elseif today == jd 
+			println("On ",year,"/",month,"/",day," there is a waxing crescent.")
+		elseif today < jd 
+			println("On ",year,"/",month,"/",day," there will be a waxing crescent.")
+		end
 	else 
-		println("On ",year,"/",month,"/",day," there was/is/will be a waning crescent.")
+		if today > jd 
+			println("On ",year,"/",month,"/",day," there was be a waning crescent.")
+		elseif today == jd 
+			println("On ",year,"/",month,"/",day," there is a waning crescent.")
+		elseif today < jd 
+			println("On ",year,"/",month,"/",day," there will be a waning crescent.")
+		end
 	end
-	println("Only ", AstroLib.mphase(jd)*100, "% of the Moon was illuminated")
+	println("Only ",  round( AstroLib.mphase(jd)*100) , "% of the Moon illuminated")
 elseif 49 <= AstroLib.mphase(jd)*100 < 51
 	if AstroLib.mphase(jd) > AstroLib.mphase(jd-1)
-		println("On ",year,"/",month,"/",day," there was/is/will be a first quarter.")
+		if today > jd 
+			println("On ",year,"/",month,"/",day," there was a first quarter.")
+		elseif today == jd 
+			println("On ",year,"/",month,"/",day," there is a first quarter.")
+		elseif today < jd 
+			println("On ",year,"/",month,"/",day," there will be a first quarter.")
+		end
 	else 
-		println("On ",year,"/",month,"/",day," there was/is/will be a last quarter.")
+		if today > jd 
+			println("On ",year,"/",month,"/",day," there was a last quarter.")
+		elseif today == jd 
+			println("On ",year,"/",month,"/",day," there is a last quarter.")
+		elseif today < jd 
+			println("On ",year,"/",month,"/",day," there will be a last quarter.")
+		end
 	end
-	println("And ", AstroLib.mphase(jd)*100, "% of the Moon was illuminated")
+	println("And ",  round( AstroLib.mphase(jd)*100) , "% of the Moon illuminated")
 elseif 51 <= AstroLib.mphase(jd)*100 < 99
 	if AstroLib.mphase(jd) > AstroLib.mphase(jd-1)
-		println("On ",year,"/",month,"/",day," there was/is/will be a waxing gibbous.")
-	else 
-		println("On ",year,"/",month,"/",day," there was/is/will be a waning gibbous.")
+		if today > jd 
+			println("On ",year,"/",month,"/",day," there was a waxing gibbous.")
+		elseif today == jd 
+			println("On ",year,"/",month,"/",day," there is a waxing gibbous.")
+		elseif today < jd 
+			println("On ",year,"/",month,"/",day," there will be a waxing gibbous.")
+		end
+	else
+		if today > jd 
+			println("On ",year,"/",month,"/",day," there was a waning gibbous.")
+		elseif today == jd 
+			println("On ",year,"/",month,"/",day," there is a waning gibbous.")
+		elseif today < jd 
+			println("On ",year,"/",month,"/",day," there will be a waning gibbous.")
+		end 
 	end
-	println("And ", AstroLib.mphase(jd)*100, "% of the Moon was illuminated")
-else println("On ",year,"/",month,"/",day," there was/is/will be a full moon.")
-
+	println("And ",  round( AstroLib.mphase(jd)*100), "% of the Moon illuminated")
+else 
+	if today > jd 
+		println("On ",year,"/",month,"/",day," there was a full moon.")
+	elseif today == jd 
+		println("On ",year,"/",month,"/",day," there is a full moon.")
+	elseif today < jd 
+		println("On ",year,"/",month,"/",day," there will be a full moon.")
+	end
 end
