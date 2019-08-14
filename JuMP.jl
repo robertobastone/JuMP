3dfb67af61c8352259a5eeb25582aea88a748222
Roberto Bastone <35118511+robertobastone@users.noreply.github.com>
2019-08-15 00:21:10 +0200

    Add files via upload

--- JuMP.jl
+++ JuMP.jl
@@ -10,40 +10,55 @@ no = ["No","no","NO","N","n"]
 
 functionalities = Dict("1" => "date", "999" => "quit")
 
-
-######################### CODE
-printstyled("Welcome to JUlia Moon Phase (JuMP) version " * string(version) * "\n",color=:blue)
-printstyled("(Author: " * author * ") \n",color=:blue)
-printstyled("For info - or anything else - please, feel free to reach me at " * email * "\n",color=:blue)
-
-while true
-	printstyled("JuMP yields several functionalities. Would you like me to describe them for you? [y/n] ",color=:blue)
-	choice = readline()
-	if any(x->x==choice, yes)
-		printstyled("By typing \"date\", JuMP will ask you a date (format yyyy/mm/dd) and tell you the moon phase of that given date. \n",color=:blue)
-		printstyled("Typing \"quit\" will terminate the execution of the script. \n",color=:blue)
-		break
-	elseif any(x->x==choice, no)
-		printstyled("Okay, let us continue. \n",color=:blue)
-		break
-	else
-		printstyled("Invalid input. \n",color=:yellow)
-		continue
-	end
+module allJuMPfunctionalities
+	include("JuMP_date.jl")
 end
 
-while true
-	println("What functionality do you choose?")
-	funct = readline()
-	if ( funct == functionalities["1"] )
-		include("JuMP_date.jl")
-		break
-	elseif( funct == functionalities["999"])
-	        printstyled("Terminating... JUlia Moon Phase version " * string(version) *"\n",color=:blue)
-		printstyled("If you find any bug, please do not hesitate to contact me at " * email * "\n",color=:blue)
-		break
-	else
-		printstyled("Invalid input. \n",color=:yellow)
-		continue
+######################### CODE
+function main()
+	introduction()
+	jumpDescription()
+	chooseFunctionality()
+end
+
+function introduction()
+	printstyled("Welcome to JUlia Moon Phase (JuMP) version " * string(version) * "\n",color=:blue)
+	printstyled("(Author: " * author * ") \n",color=:blue)
+	printstyled("For info - or anything else - please, feel free to reach me at " * email * "\n",color=:blue)
+end
+
+function jumpDescription()
+	while true
+		printstyled("JuMP yields several functionalities. Would you like me to describe them for you? [y/n] ",color=:blue)
+		choice = readline()
+		if any(x->x==choice, yes)
+			printstyled("By typing \"date\", JuMP will ask you a date (format yyyy/mm/dd) and tell you the moon phase of that given date. \n",color=:blue)
+			printstyled("Typing \"quit\" will terminate the execution of the script. \n",color=:blue)
+			break
+		elseif any(x->x==choice, no)
+			printstyled("Okay, let us continue. \n",color=:blue)
+			break
+		else
+			printstyled("Invalid input. \n",color=:yellow)
+			continue
+		end
+	end
+end 
+
+function chooseFunctionality()
+	while true
+		println("What functionality do you choose?")
+		funct = readline()
+		if ( funct == functionalities["1"] )
+			allJuMPfunctionalities.main()
+			break
+		elseif( funct == functionalities["999"])
+			printstyled("Terminating... JUlia Moon Phase version " * string(version) *"\n",color=:blue)
+			printstyled("If you find any bug, please do not hesitate to contact me at " * email * "\n",color=:blue)
+			break
+		else
+			printstyled("Invalid input. \n",color=:yellow)
+			continue
+		end
 	end
 end
--- JuMP_date.jl
+++ JuMP_date.jl
@@ -3,7 +3,7 @@
 # author: Roberto Bastone
 # email: robertobastone93@gmail.com
 
-version = 1.01
+version = 1.02
 
 ######################### IMPORT
 
@@ -18,90 +18,183 @@ crescent_moon = 49
 quarter_moon = 51
 gibbous_moon = 99
 
-printstyled("JuMP is going to tell the phase of the moon for a certain day. \n",color=:blue)
-printstyled("Type the year (format yyyy) \n",color=:blue)
-year = readline()
-year = parse(Int32, year)
-printstyled("Type the month (format mm) \n",color=:blue)
-month = readline()
-month = parse(Int32, month)
-printstyled("Type the day (format dd) \n",color=:blue)
-day = readline()
-day = parse(Int32, day)
-jd = jdcnv(year, month, day)
 
+function main()
+	julianInputDate, inputDate = choosingTheDate()
+	lookingAtTheMoon(today,julianInputDate, inputDate)
+end
 
-if 0 < AstroLib.mphase(jd)*100 < new_moon
-	if today > jd
-		println("On ",year,"/",month,"/",day," there was a new moon.")
-	elseif today == jd
-		println("On ",year,"/",month,"/",day," there is be a new moon.")
-	elseif today < jd
-		println("On ",year,"/",month,"/",day," there will be a new moon.")
+function choosingTheDate()
+	printstyled("JuMP is going to tell the phase of the moon for a certain day. \n",color=:blue)
+	year = chooseTheYear()
+	month = chooseTheMonth()
+	day = chooseTheDay()
+	#printstyled("$year - $month - $day",color=:blue)
+	date = Dict("year" => year, "month" => month, "day" => day)
+	jd = jdcnv(year, month, day)
+	return jd, date
+end
+
+function chooseTheYear()
+	year = 2019
+	while true
+		try
+			printstyled("Type the year (format yyyy) \n",color=:blue)
+			year = readline()
+			year = parse(Int32, year)
+			break
+		catch
+			printstyled("This is not an integer. \n",color=:red)
+			continue	
+		end
 	end
-elseif new_moon <= AstroLib.mphase(jd)*100 < crescent_moon
+	return year
+end
+
+function chooseTheMonth()
+	month = 08
+	while true
+		try
+			printstyled("Type the month (format mm) \n",color=:blue)
+			month = readline()
+			month = parse(Int32, month)
+			if month > 13 || month < 0
+				printstyled("Bad value for month. \n",color=:red)
+				continue
+			end
+			break
+		catch
+			printstyled("This is not an integer. \n",color=:red)
+			continue	
+		end
+	end
+	return month
+end
+
+function chooseTheDay()
+	day = 15
+	while true
+		try
+			printstyled("Type the day (format dd) \n",color=:blue)
+			day = readline()
+			day = parse(Int32, day)
+			break
+		catch
+			printstyled("This is not an integer. \n",color=:red)
+			continue	
+		end
+	end
+	return day
+end
+
+function lookingAtTheMoon(today,jd, inputDate)
+	if 0 < AstroLib.mphase(jd)*100 < new_moon
+		newMoon(today, jd, inputDate)
+	elseif new_moon <= AstroLib.mphase(jd)*100 < crescent_moon
+		crescentMoon(today,jd, inputDate)
+	elseif crescent_moon <= AstroLib.mphase(jd)*100 < quarter_moon
+		quarterMoon(today,jd, inputDate)
+	elseif quarter_moon <= AstroLib.mphase(jd)*100 < gibbous_moon
+		gibbousMoon(today,jd, inputDate)
+	else 
+		fullMoon(today,jd, inputDate)
+	end
+end
+
+function newMoon(today, jd, inputDate)
+	y = inputDate["year"]
+	m = inputDate["month"]
+	d = inputDate["day"]
+	if today > jd
+		println("On ",y,"/",m,"/",d," there was a new moon.")
+	elseif today == jd
+		println("On ",y,"/",m,"/",d," there is be a new moon.")
+	elseif today < jd
+		println("On ",y,"/",m,"/",d," there will be a new moon.")
+	end
+end
+
+function crescentMoon(today, jd, inputDate)
+	y = inputDate["year"]
+	m = inputDate["month"]
+	d = inputDate["day"]
 	if AstroLib.mphase(jd) > AstroLib.mphase(jd-1)
-		if today > jd 
-			println("On ",year,"/",month,"/",day," there was be a waxing crescent.")
-		elseif today == jd 
-			println("On ",year,"/",month,"/",day," there is a waxing crescent.")
-		elseif today < jd 
-			println("On ",year,"/",month,"/",day," there will be a waxing crescent.")
+		if today > jd
+			println("On ",y,"/",m,"/",d," there was be a waxing crescent.")
+		elseif today == inputDate 
+			println("On ",y,"/",m,"/",d," there is a waxing crescent.")
+		elseif today < inputDate 
+			println("On ",y,"/",m,"/",d," there will be a waxing crescent.")
 		end
 	else 
-		if today > jd 
-			println("On ",year,"/",month,"/",day," there was be a waning crescent.")
-		elseif today == jd 
-			println("On ",year,"/",month,"/",day," there is a waning crescent.")
-		elseif today < jd 
-			println("On ",year,"/",month,"/",day," there will be a waning crescent.")
+		if today > inputDate 
+			println("On ",y,"/",m,"/",d," there was be a waning crescent.")
+		elseif today == inputDate 
+			println("On ",y,"/",m,"/",d," there is a waning crescent.")
+		elseif today < inputDate 
+			println("On ",y,"/",m,"/",d," there will be a waning crescent.")
 		end
 	end
 	println("Only ",  round( AstroLib.mphase(jd)*100) , "% of the Moon illuminated")
-elseif crescent_moon <= AstroLib.mphase(jd)*100 < quarter_moon
+end
+
+function quarterMoon(today, jd, inputDate)
+	y = inputDate["year"]
+	m = inputDate["month"]
+	d = inputDate["day"]
 	if AstroLib.mphase(jd) > AstroLib.mphase(jd-1)
 		if today > jd 
-			println("On ",year,"/",month,"/",day," there was a first quarter.")
-		elseif today == jd 
-			println("On ",year,"/",month,"/",day," there is a first quarter.")
-		elseif today < jd 
-			println("On ",year,"/",month,"/",day," there will be a first quarter.")
+			println("On ",y,"/",m,"/",d," there was a first quarter.")
+		elseif today == inputDate 
+			println("On ",y,"/",m,"/",d," there is a first quarter.")
+		elseif today < inputDate 
+			println("On ",y,"/",m,"/",d," there will be a first quarter.")
 		end
 	else 
 		if today > jd 
-			println("On ",year,"/",month,"/",day," there was a last quarter.")
-		elseif today == jd 
-			println("On ",year,"/",month,"/",day," there is a last quarter.")
+			println("On ",y,"/",m,"/",d," there was a last quarter.")
+		elseif today == jd
+			println("On ",y,"/",m,"/",d," there is a last quarter.")
 		elseif today < jd 
-			println("On ",year,"/",month,"/",day," there will be a last quarter.")
+			println("On ",y,"/",m,"/",d," there will be a last quarter.")
 		end
 	end
 	println("And ",  round( AstroLib.mphase(jd)*100) , "% of the Moon illuminated")
-elseif quarter_moon <= AstroLib.mphase(jd)*100 < gibbous_moon
+end 
+
+function gibbousMoon(today, jd, inputDate)
+	y = inputDate["year"]
+	m = inputDate["month"]
+	d = inputDate["day"]	
 	if AstroLib.mphase(jd) > AstroLib.mphase(jd-1)
 		if today > jd 
-			println("On ",year,"/",month,"/",day," there was a waxing gibbous.")
+			println("On ",y,"/",m,"/",d," there was a waxing gibbous.")
 		elseif today == jd 
-			println("On ",year,"/",month,"/",day," there is a waxing gibbous.")
+			println("On ",y,"/",m,"/",d," there is a waxing gibbous.")
 		elseif today < jd 
-			println("On ",year,"/",month,"/",day," there will be a waxing gibbous.")
+			println("On ",y,"/",m,"/",d," there will be a waxing gibbous.")
 		end
 	else
 		if today > jd 
-			println("On ",year,"/",month,"/",day," there was a waning gibbous.")
+			println("On ",y,"/",m,"/",d," there was a waning gibbous.")
 		elseif today == jd 
-			println("On ",year,"/",month,"/",day," there is a waning gibbous.")
+			println("On ",y,"/",m,"/",d," there is a waning gibbous.")
 		elseif today < jd 
-			println("On ",year,"/",month,"/",day," there will be a waning gibbous.")
+			println("On ",y,"/",m,"/",d," there will be a waning gibbous.")
 		end 
 	end
 	println("And ",  round( AstroLib.mphase(jd)*100), "% of the Moon illuminated")
-else 
+end
+
+function fullMoon(today, jd, inputDate)
+	y = inputDate["year"]
+	m = inputDate["month"]
+	d = inputDate["day"]
 	if today > jd 
-		println("On ",year,"/",month,"/",day," there was a full moon.")
+		println("On ",y,"/",m,"/",d," there was a full moon.")
 	elseif today == jd 
-		println("On ",year,"/",month,"/",day," there is a full moon.")
+		println("On ",y,"/",m,"/",d," there is a full moon.")
 	elseif today < jd 
-		println("On ",year,"/",month,"/",day," there will be a full moon.")
+		println("On ",y,"/",m,"/",d," there will be a full moon.")
 	end
 end
